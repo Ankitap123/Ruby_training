5f7634e7cf6d30094d08bad430b0482260b17e0a
Ankitap123 <ankitapandey141296@gmail.com>
2019-01-13 23:24:04 +0530

    Class assignment momdified

--- Class_calculator.rb
+++ Class_calculator.rb
@@ -1,39 +1,72 @@
 class Calculator
-	def sum(num1,num2)
-		puts"#{num1+num2}"
+	attr_accessor :num1, :num2
+	
+	def initialize(num1,num2)
+		@num1 = num1
+		@num2 = num2
+	end	
+	def sum()
+		@num1 + @num2
 	end
-	def mult(num1,num2)
-		puts"#{num1*num2}"
+	def mult()
+		@num1 * @num2
 	end
 	def div(num1,num2)
 
-		puts"#{(num1/num2).to_f}" 
+		@num1 / @num2
 	end
 	def subt(num1,num2)
-
-		puts"#{(num1-num2).to_f}" 
+	    @num1 - @num2
 	end
-	def common_met(num1,num2,op)
-		a = [num1,num2]
-        result=a.inject(op.chomp.to_sym)
-        puts result
+	def common_method(op)
+		case op
+		when "+"
+			puts "Addition is :#{sum} "
+		when "-"
+			puts "Subtraction is :#{subt} "
+		when "*"
+			puts "Multiplication is :#{mult} "
+		when  "/"
+			puts "Division is :#{div} "
+		end
 	end
-	def self.display_details()
-		puts 'Operations are:'
-		puts '1.+ 2.- 3.* 4./'
+	def operator_checking(op)
+		if (op.eql?("+") || op.eql?("-") || op.eql?("*")|| op.eql?("/"))
+			return true
+		end
 	end
-
+	def display_op()
+		puts ' List Of Operations { \'1. + \' , \' 2.- \' , \'3. * \' , \' 4. / \' }'
+		puts 'Enter operator'
+		op = gets.chomp
+		if operator_checking( op )
+			common_method( op )
+		else
+			puts 'Operator entered is invalid'
+			return
+		end
+	end
 end
 
-Calculator.display_details()
-obj1=Calculator.new
-obj2=Calculator.new
-obj3=Calculator.new
-obj4=Calculator.new
-obj1.sum(3,5)
-obj2.mult(3,5)
-obj3.div(8,5)
-obj4.subt(3,5)
-obj5=Calculator.new
-obj5.common_met(3,4,'+')
+input=0
+loop do
 
+	begin
+		puts 'Operand 1'
+		num1 = Integer(gets.chomp)
+		puts 'Operand 2'
+		num2 = Integer(gets.chomp)
+		num1= num1.to_f
+		num2 = num2.to_f
+	rescue
+		puts 'Invalid Operand'
+		retry
+	end
+	
+	obj1 = Calculator.new(num1, num2)
+	obj1.display_op()
+	puts 'Exit: 0 else Continue: 1'
+	input= gets.chomp.to_i
+	break if input == 0
+
+end
